d84c2fb109a26e7806db4cc54c8e30fb2801d0f4
wolpi <wolpi.dev@web.de>
2024-05-01 17:35:10 +0200

    extended parsing to be able to deal with dates instead of timestamps and with optional quotes as well as with integers instead of floats

--- src/parse.rs
+++ src/parse.rs
@@ -1,11 +1,13 @@
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 use chrono::NaiveDateTime;
+use chrono::NaiveDate;
 use chrono::format::DelayedFormat;
 use chrono::format::StrftimeItems;
 use serde::{Serialize, Serializer};
 
 const TIMESTAMP_FORMAT :&str = "%Y-%m-%d %H:%M:%S";
+const DATE_FORMAT :&str = "%Y-%m-%d";
 const SEPARATOR :&str = ",";
 
 #[derive(Clone)]
@@ -113,21 +115,53 @@ fn parse_line(line :&String, line_counter:&u32) -> (Entry, bool) {
 }
 
 fn parse_timestamp(line :&String, entry :&mut Entry, start_index :&usize) -> Result<usize, &'static str> {
-    let line_remainer = &line[*start_index +1 .. line.len()]; // +1 for quote
+    let first_char_result = (*line).chars().nth(0);
+    if first_char_result.is_none() {
+        return Result::Err("empty value");
+    }
+    let quotes_present = first_char_result.unwrap() == '"';
+    let local_start_index = match quotes_present {
+        true => *start_index + 1,
+        false => *start_index
+    };
+    let end_index_offset =  match quotes_present {
+        true => 1,
+        false => 0
+    };
+
+    let line_remainer = &line[local_start_index .. line.len()];
     let find_result = line_remainer.find(SEPARATOR);
     let err_msg = "could not parse: timestamp";
     return if find_result.is_some() {
         let index = find_result.unwrap();
+        let success_index = local_start_index + index + 1; // ignore end_index_offset, +1 for csv separator
         if index > 0 {
-            let timestamp_str: &str = &line_remainer[0 .. index -1];
+            let timestamp_str: &str = &line_remainer[0 .. index - end_index_offset];
             let parse_result = NaiveDateTime::parse_from_str(timestamp_str, TIMESTAMP_FORMAT);
             if parse_result.is_ok() {
                 let timestamp = parse_result.unwrap();
                 entry.timestamp = WeightDateTime::new(timestamp);
                 entry.raw_timestamp = timestamp.timestamp_millis() as u64;
-                Result::Ok(*start_index +1 + index)
+                Result::Ok(success_index)
             } else {
-                Result::Err(err_msg)
+                // try date format
+                let date_result = NaiveDate::parse_from_str(timestamp_str, DATE_FORMAT);
+                if date_result.is_ok() {
+                    let date = date_result.unwrap();
+                    let millis_opt = date.and_hms_milli_opt(0, 0, 0, 0);
+                    if millis_opt.is_some() {
+                        let millis = millis_opt.unwrap().timestamp_millis();
+                        let secs = millis / 1000;
+                        let timestamp = NaiveDateTime::from_timestamp(secs, 0);
+                        entry.timestamp = WeightDateTime::new(timestamp);
+                        entry.raw_timestamp = millis as u64;
+                        Result::Ok(success_index)
+                    } else {
+                        Result::Err(err_msg)
+                    }
+                } else {
+                    Result::Err(err_msg)
+                }
             }
         } else {
             Result::Err(err_msg)
@@ -138,26 +172,27 @@ fn parse_timestamp(line :&String, entry :&mut Entry, start_index :&usize) -> Res
 }
 
 fn parse_float(line :&String, target :&mut f32, start_index :&usize, err_msg :&'static str) -> Result<usize, &'static str> {
-    let line_remainer = &line[*start_index +1 .. line.len()];
+    let line_remainer = &line[*start_index .. line.len()];
+    //println!("float remainer: {}", line_remainer);
     let find_result = line_remainer.find(SEPARATOR);
-    return if find_result.is_some() {
-        let index = find_result.unwrap();
-        if index > 0 {
-            let mut int_str = &line_remainer[0..index];
-            let decimal_result = int_str.find(",");
-            if decimal_result.is_some() {
-                let decimal_index = decimal_result.unwrap();
-                if decimal_index > 0 {
-                    int_str = &line_remainer[0..decimal_index];
-                }
+    let index = if find_result.is_some() {
+        find_result.unwrap()
+    } else {
+        line_remainer.len()
+    };
+    return if index > 0 {
+        let mut int_str = &line_remainer[0..index];
+        let decimal_result = int_str.find(",");
+        if decimal_result.is_some() {
+            let decimal_index = decimal_result.unwrap();
+            if decimal_index > 0 {
+                int_str = &line_remainer[0..decimal_index];
             }
-            let parse_result = int_str.parse::<f32>();
-            if parse_result.is_ok() {
-                *target = parse_result.unwrap();
-                Result::Ok(*start_index +1 + index)
-            } else {
-                Result::Err(err_msg)
-            }
+        }
+        let parse_result = int_str.parse::<f32>();
+        if parse_result.is_ok() {
+            *target = parse_result.unwrap();
+            Result::Ok(*start_index +1 + index)
         } else {
             Result::Err(err_msg)
         }
